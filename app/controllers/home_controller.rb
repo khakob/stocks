749dab8773ef8f78d2c5c55afaf978edbe089c39
khakob <khakobian@geminiduplication.com>
2019-07-11 14:34:24 -0700

    Minor fixes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,14 @@
 class HomeController < ApplicationController
   def index
     @api = StockQuote::Stock.new(api_key: 'pk_16a849fd637243a79fff90fa4d42bc5d')
-    
     if params[:ticker] == ""
       @nothing = "Hey! Your forgot to enter a symbol"
     elsif params[:ticker]
-      @stock = Stock.where(ticker: params[:ticker])
-      if !@stock 
-  			@error = "Hey! That Stock Symbol Doesn't Exist. Please Try Again."
+      begin
+        @quote = StockQuote::Stock.quote(params[:ticker])
+        @stats = StockQuote::Stock.stats(params[:ticker])
+      rescue => e
+        @error ="Hey! That Stock Symbol Doesn't Exist. Please Try Again."
   		end
     end
   end
